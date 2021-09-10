72280a5c56dc6cf84d04bfe3eff77b072244a634
duan-jia <duanjiabingcom@163.com>
2021-09-10 14:16:30 +0800

    移植开源项目seasky的源码

---
+++ SerialScope/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+const QString SerialScopeVersion = QString::fromUtf16(u"fyt串口调试助手 V0.0.1");
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    QFile file(":/qss/vQss/vQss.css");
+    if (file.open(QFile::ReadOnly))
+    {
+        QTextStream filetext(&file);
+        QString stylesheet = filetext.readAll();
+        qApp->setStyleSheet(stylesheet);
+    }
+    a.setWindowIcon(QIcon(":/image/image/main.ico"));
+    w.setWindowTitle(SerialScopeVersion);
+    w.show();
+    return a.exec();
+}
